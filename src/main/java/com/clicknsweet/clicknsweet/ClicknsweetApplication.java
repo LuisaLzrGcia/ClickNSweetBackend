57646a840a50c121a096262e5478d7ea71e63c00
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-08-05 13:30:21 -0600

    first commit

---
+++ src/main/java/com/clicknsweet/clicknsweet/ClicknsweetApplication.java
@@ -0,0 +1,13 @@
+package com.clicknsweet.clicknsweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicknsweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClicknsweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clicknsweet/clicknsweet/ClicknsweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clicknsweet.clicknsweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicknsweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

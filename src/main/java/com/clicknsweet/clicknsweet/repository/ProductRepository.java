4c5a2bfdb9f9606415bb718080bf298b4565a855
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-08-06 15:50:23 -0600

    CRUD de producto: get, create, update y delete con manejo de errores

--- src/main/java/com/clicknsweet/clicknsweet/repository/ProductRepository.java
+++ src/main/java/com/clicknsweet/clicknsweet/repository/ProductRepository.java
@@ -1,4 +1,13 @@
 package com.clicknsweet.clicknsweet.repository;
 
-public class ProductRepository {
+import com.clicknsweet.clicknsweet.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    // Buscra porducto por nombre
+    @Query("SELECT p FROM Product p WHERE p.productName =?1")
+    Product  findByName(String name);
 }
